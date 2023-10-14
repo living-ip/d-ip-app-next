0af889f36241e8fb8bdede016e7094ab2c269dbe
SolusRGB <brandoneddycode@gmail.com>
2023-10-15 00:44:31 +0100

    Refractor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,19 @@
 import '@/styles/globals.css'
-import {createStytchUIClient} from "@stytch/nextjs/ui";
-import {StytchProvider} from "@stytch/nextjs";
-import Wallet from "@/components/Wallet";
+import { createStytchUIClient } from '@stytch/nextjs/ui'
+import { StytchProvider } from '@stytch/nextjs'
+import Wallet from '@/components/Wallet'
 
 const stytch = createStytchUIClient(
-    process.env.STYTCH_PUBLIC_TOKEN || "public-token-test-0d6d430c-8503-41d0-b4bb-97b6a448f6ac"
-);
+	process.env.STYTCH_PUBLIC_TOKEN ||
+		'public-token-test-0d6d430c-8503-41d0-b4bb-97b6a448f6ac'
+)
 
 export default function App({ Component, pageProps }) {
-  return (<>
-            <StytchProvider stytch={stytch}>
-                    <Component {...pageProps} />
-            </StytchProvider>
-        </>
-    )
+	return (
+		<>
+			<StytchProvider stytch={stytch}>
+				<Component {...pageProps} />
+			</StytchProvider>
+		</>
+	)
 }
