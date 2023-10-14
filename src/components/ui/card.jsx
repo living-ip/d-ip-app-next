7c750113fa5832a677fe424a5ed741f8d08539ef
SolusRGB <brandoneddycode@gmail.com>
2023-10-15 00:43:09 +0100

    Update CSS, add Image const

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,50 +1,92 @@
-import * as React from "react"
+import * as React from 'react'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
 
 const Card = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
-    {...props} />
+	<div
+		ref={ref}
+		className={cn(
+			'rounded-lg border bg-card text-card-foreground drop-shadow-lg min-w-[400px] max-w-[400px] min-h-[180px]',
+			className
+		)}
+		{...props}
+	/>
 ))
-Card.displayName = "Card"
+Card.displayName = 'Card'
 
 const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
-    {...props} />
+	<div
+		ref={ref}
+		className={cn('flex flex-col space-y-1.5 p-8', className)}
+		{...props}
+	/>
 ))
-CardHeader.displayName = "CardHeader"
+CardHeader.displayName = 'CardHeader'
 
 const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn("text-2xl font-semibold leading-none tracking-tight", className)}
-    {...props} />
+	<h3
+		ref={ref}
+		className={cn(
+			'text-3xl font-semibold mb-6 leading-none tracking-tight',
+			className
+		)}
+		{...props}
+	/>
 ))
-CardTitle.displayName = "CardTitle"
+CardTitle.displayName = 'CardTitle'
+
+const CardImage = React.forwardRef(({ src, alt, className, ...props }, ref) => (
+	<div
+		ref={ref}
+		className={cn(
+			'relative w-full h-full overflow-hidden flex items-center justify-center',
+			className
+		)}
+		{...props}
+	>
+		<Image
+			src={src}
+			alt={alt}
+			width={320}
+			height={180}
+			objectFit="cover"
+			objectPosition="center"
+			className="rounded-lg"
+		/>
+	</div>
+))
+CardImage.displayName = 'CardImage'
 
 const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
-  <p
-    ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
-    {...props} />
+	<p
+		ref={ref}
+		className={cn('text-sm py-6 text-muted-foreground', className)}
+		{...props}
+	/>
 ))
-CardDescription.displayName = "CardDescription"
+CardDescription.displayName = 'CardDescription'
 
 const CardContent = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+	<div ref={ref} className={cn('p-6 pt-0', className)} {...props} />
 ))
-CardContent.displayName = "CardContent"
+CardContent.displayName = 'CardContent'
 
 const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
-    {...props} />
+	<div
+		ref={ref}
+		className={cn('flex items-center p-6 pt-0', className)}
+		{...props}
+	/>
 ))
-CardFooter.displayName = "CardFooter"
+CardFooter.displayName = 'CardFooter'
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export {
+	Card,
+	CardHeader,
+	CardFooter,
+	CardTitle,
+	CardDescription,
+	CardContent,
+	CardImage,
+}
