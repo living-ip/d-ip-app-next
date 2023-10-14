c07b21e7c730629616775acbaac7c07d3d3ae4ba
SolusRGB <brandoneddycode@gmail.com>
2023-10-15 00:47:52 +0100

    Route change to /publications

--- src/pages/onboard.js
+++ src/pages/onboard.js
@@ -1,48 +1,48 @@
-import {Inter} from 'next/font/google'
-import NavBar from "@/components/NavBar";
-import {RegisterCard} from "@/components/RegisterCard";
-import {authStytchRequest} from "@/lib/stytch";
-import { getUserProfile } from '@/lib/user';
+import { Inter } from 'next/font/google'
+import NavBar from '@/components/NavBar'
+import { RegisterCard } from '@/components/RegisterCard'
+import { authStytchRequest } from '@/lib/stytch'
+import { getUserProfile } from '@/lib/user'
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 
-export default function Onboard({user}) {
-    return (
-        <NavBar>
-            <div className="flex items-center justify-center min-h-screen bg-gray-100">
-                <RegisterCard/>
-            </div>
-        </NavBar>
-    )
+export default function Onboard({ user }) {
+	return (
+		<NavBar>
+			<div className="flex items-center justify-center min-h-screen bg-gray-100">
+				<RegisterCard />
+			</div>
+		</NavBar>
+	)
 }
 
-export const getServerSideProps = async ({req, query}) => {
-    const noAuthResponse = {
-        redirect: {
-            destination: "/login",
-            permanent: false,
-        },
-    };
-    try {
-        // Authenticate the session JWT. If an error is thrown the session authentication has failed.
-        const session = await authStytchRequest(req)
-        if (!session) {
-            return noAuthResponse
-        }
-        const { userProfile } = await getUserProfile(session.user_id)
-        if (userProfile) {
-            return {
-                redirect: {
-                    destination: "/discover",
-                    permanent: false,
-                }
-            }
-        }
-        return {
-            props: {}
-        }
-    } catch (e) {
-        console.error(e)
-        return noAuthResponse;
-    }
+export const getServerSideProps = async ({ req, query }) => {
+	const noAuthResponse = {
+		redirect: {
+			destination: '/login',
+			permanent: false,
+		},
+	}
+	try {
+		// Authenticate the session JWT. If an error is thrown the session authentication has failed.
+		const session = await authStytchRequest(req)
+		if (!session) {
+			return noAuthResponse
+		}
+		const { userProfile } = await getUserProfile(session.user_id)
+		if (userProfile) {
+			return {
+				redirect: {
+					destination: '/publications',
+					permanent: false,
+				},
+			}
+		}
+		return {
+			props: {},
+		}
+	} catch (e) {
+		console.error(e)
+		return noAuthResponse
+	}
 }
