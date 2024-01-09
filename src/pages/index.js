f04d5d07aedd88d9f9829c4c62790ad5cac36e2e
Tom Davies <tom@sibylline.xyz>
2024-01-09 12:35:36 +0000

    navigation refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,29 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { authStytchRequest } from '@/lib/stytch'
+import {Inter} from 'next/font/google'
+import {authStytchRequest} from '@/lib/stytch'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
-	return (
-		<div>
-			<h1>Home</h1>
-		</div>
-	)
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
 }
 
-export const getServerSideProps = async ({ req }) => {
-	const session = await authStytchRequest(req)
-	if (!session) {
-		return {
-			redirect: {
-				destination: '/login',
-				permanent: false,
-			},
-		}
-	}
-	return {
-		redirect: {
-			destination: '/publications',
-		},
-	}
+export const getServerSideProps = async ({req}) => {
+  const session = await authStytchRequest(req)
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+  return {
+    redirect: {
+      destination: '/collections',
+    },
+  }
 }
