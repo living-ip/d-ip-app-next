bb1577596b9fc1c597078a10eadc880410cf9318
Tom Davies <tom@sibylline.xyz>
2024-01-10 12:53:18 +0000

    Fixing deployment

--- src/pages/api/collection/[collectionId]/document/index.js
+++ src/pages/api/collection/[collectionId]/document/index.js
@@ -1,3 +1,7 @@
+// TODO create a new document in this collection on a POST request.
+// this requires creating a new doc in the DB, marking the owner as the requestor, and making a new repo for the content
+// in github.
+
 import {authStytchToken} from "@/lib/stytch";
 
 const handler = async (req, res) => {
