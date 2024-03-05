b122720339f1e02b93b6a8318985e14dd1e962ba
Ben Harper <ben@harpoon.dev>
2024-03-05 18:11:19 +0000

    add prisma import

--- src/pages/collections/[name]/new.js
+++ src/pages/collections/[name]/new.js
@@ -14,6 +14,7 @@ import {
   AlertDialogTitle
 } from "@/components/ui/alert-dialog";
 import {useState} from "react";
+import prisma from "@/lib/server/prisma";
 
 export default function CreateNewDocument({collection}) {
   const router = useRouter();
