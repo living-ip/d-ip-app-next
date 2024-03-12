2e0a5bb18d50566d76b40ef4238a0bd6a2c04f59
Ben Harper <ben@harpoon.dev>
2024-03-12 16:04:34 +0000

    merge PR if positive votes and change is over 24 hours old

--- src/pages/api/change/merge-changes.js
+++ src/pages/api/change/merge-changes.js
@@ -1,6 +1,53 @@
-export default function handler(req, res) {
+import { mergePullRequest } from "@/lib/server/github";
+import prisma from "@/lib/server/prisma";
+
+const handler = async (req, res) => {
   if (req.headers['authorization'] !== `Bearer ${process.env.CRON_SECRET}`) {
     return res.status(401).end('Unauthorized');
   }
-  res.status(200).end('Hello Cron!');
+  const changes = await prisma.Change.findMany({
+    where: {
+      merged: false,
+      publishedAt: {
+        lt: new Date(new Date() - 24 * 60 * 60 * 1000), // get changes that were published more than 24 hours ago
+      }
+    },
+  });
+  for (const change of changes) {
+    const voteAggregate = await prisma.Vote.aggregate({
+      where: {
+        changeId: change.cid,
+      },
+      _sum: {
+        vote: true,
+      },
+    })
+    if (voteAggregate._sum.vote < 0) { // if the change has more downvotes than upvotes, don't merge it
+      // TODO: close the pull request
+      continue;
+    }
+    const document = await prisma.Document.findFirst({
+      where: {
+        did: change.documentId,
+      },
+    });
+    const ghResponse = await mergePullRequest(
+      document.owner,
+      document.repo,
+      change.prNumber
+    );
+    console.log(ghResponse);
+    const updatedChange = await prisma.Change.update({
+      where: {
+        cid: change.cid,
+      },
+      data: {
+        merged: true,
+      },
+    });
+    console.log(updatedChange);
+  }
+  return res.status(200).json({changes});
 }
+
+export default handler
