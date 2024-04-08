cd122e6d478cd388e2a32eef8255325bb3a10e0c
Dan Miles <dan@kilometre.dev>
2024-04-08 19:21:01 +0100

    solana connect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,32 @@
 import '@/styles/globals.css'
-import { createStytchUIClient } from '@stytch/nextjs/ui'
-import { StytchProvider } from '@stytch/nextjs'
-import Wallet from '@/components/Wallet'
+import {createStytchUIClient} from '@stytch/nextjs/ui'
+import {StytchProvider} from '@stytch/nextjs'
+import {DynamicContextProvider} from "@dynamic-labs/sdk-react-core";
+import {SolanaWalletConnectors} from "@dynamic-labs/solana";
+import Head from "next/head";
 
 const stytch = createStytchUIClient(
 	process.env.STYTCH_PUBLIC_TOKEN ||
-		'public-token-test-0d6d430c-8503-41d0-b4bb-97b6a448f6ac'
+	'public-token-test-0d6d430c-8503-41d0-b4bb-97b6a448f6ac'
 )
 
-export default function App({ Component, pageProps }) {
+export default function App({Component, pageProps}) {
 	return (
 		<>
-			<StytchProvider stytch={stytch}>
-				<Component {...pageProps} />
-			</StytchProvider>
+			<Head>
+				<title>Living IP</title>
+				<link rel="icon" href="https://storage.googleapis.com/syb_us_cdn/sibylline_favicon.png"/>
+			</Head>
+			<DynamicContextProvider
+				theme={"auto"}
+				settings={{
+					environmentId: "17eae500-ba75-4c6c-a7ae-fbc3049c5178",
+					walletConnectors: [SolanaWalletConnectors],
+				}}>
+					<StytchProvider stytch={stytch}>
+						<Component {...pageProps} />
+					</StytchProvider>
+			</DynamicContextProvider>
 		</>
 	)
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,6 +2,8 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { useStytchUser } from '@stytch/nextjs'
 import Login from '@/components/simple/Login'
+import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core';
+
 
 // In this app the index route (/) is a login page. We use the Stytch Next.js SDK to redirect the user if they are already logged in.
 export default function LoginPage() {
@@ -16,6 +18,7 @@ export default function LoginPage() {
 
 	return (
 		<div className="flex items-center justify-center min-h-screen bg-gray-100">
+			<DynamicWidget/>
 			<Login />
 		</div>
 	)
