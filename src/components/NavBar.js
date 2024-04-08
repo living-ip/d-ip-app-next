6abb3b28811d78fa27b64b3f86ce44370f0ed6d9
Ben Harper <ben@harpoon.dev>
2024-04-08 20:39:05 +0100

    Add solana wallet (#28)
    * solana connect
    
    * fix package.json
    
    * put connect wallet in the navbar
    
    * package-lock
    
    ---------
    
    Co-authored-by: Dan Miles <dan@kilometre.dev>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,29 +3,35 @@ import React from 'react'
 import Image from 'next/image'
 import {useRouter} from "next/router";
 import {Button} from "@/components/ui/button";
+import {DynamicWidget} from "@dynamic-labs/sdk-react-core";
 
 export default function NavBar() {
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <div className={"flex pb-8 border-b border-gray-200 mx-auto max-w-full px-4 sm:px-8 lg:px-16"}>
-      <NavigationMenu>
-        <NavigationMenuItem className={"flex hover:cursor-pointer"} onClick={() => router.push('/')}>
-          <Image
-            src="/living-ip.png"
-            className="hidden sm:mr-4 sm:flex"
-            width={40}
-            height={40}
-            alt="LivingIP"
-          />
-          <div className="text-2xl font-bold">LivingIP</div>
-        </NavigationMenuItem>
-      </NavigationMenu>
-      <div className="flex-1"/>
-      <Button variant="outline" className="mt-8"
-              onClick={() => router.push('/collections/LivingIP%20Product/document/cb26aba8-8188-4eb3-867c-2661c260b29c')}>
-        User Guide
-      </Button>
-    </div>
-  )
+	return (
+		<div className={"flex pb-8 border-b border-gray-200 mx-auto max-w-full px-4 sm:px-8 lg:px-16"}>
+			<NavigationMenu>
+				<NavigationMenuItem className={"flex hover:cursor-pointer"} onClick={() => router.push('/')}>
+					<Image
+						src="/living-ip.png"
+						className="hidden sm:mr-4 sm:flex"
+						width={40}
+						height={40}
+						alt="LivingIP"
+					/>
+					<div className="text-2xl font-bold">LivingIP</div>
+				</NavigationMenuItem>
+			</NavigationMenu>
+			<div className="flex-1"/>
+			<div className="flex mt-8 gap-x-4">
+				<Button variant="outline"
+				        onClick={() => router.push('/collections/LivingIP%20Product/document/cb26aba8-8188-4eb3-867c-2661c260b29c')}>
+					User Guide
+				</Button>
+				<DynamicWidget
+					innerButtonComponent={<Button>Connect Wallet</Button>}
+				/>
+			</div>
+		</div>
+	)
 }
