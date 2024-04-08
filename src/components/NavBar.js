481d39314493082aebded17ecdca3897943e3297
Tom <131542622+tcd3@users.noreply.github.com>
2024-04-08 16:34:56 +0100

    User Guide button added to Navbar (#25)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,13 @@ import {NavigationMenu, NavigationMenuItem,} from '@/components/ui/navigation-me
 import React from 'react'
 import Image from 'next/image'
 import {useRouter} from "next/router";
+import {Button} from "@/components/ui/button";
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
-    <div className={"pb-8 border-b border-gray-200 mx-auto max-w-full px-4 sm:px-8 lg:px-16"}>
+    <div className={"flex pb-8 border-b border-gray-200 mx-auto max-w-full px-4 sm:px-8 lg:px-16"}>
       <NavigationMenu>
         <NavigationMenuItem className={"flex hover:cursor-pointer"} onClick={() => router.push('/')}>
           <Image
@@ -20,6 +21,11 @@ export default function NavBar() {
           <div className="text-2xl font-bold">LivingIP</div>
         </NavigationMenuItem>
       </NavigationMenu>
+      <div className="flex-1"/>
+      <Button variant="outline" className="mt-8"
+              onClick={() => router.push('/collections/LivingIP%20Product/document/cb26aba8-8188-4eb3-867c-2661c260b29c')}>
+        User Guide
+      </Button>
     </div>
   )
 }
