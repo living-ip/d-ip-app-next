6748592dafd5555c28322c5e7cd59f984b9503f7
Tom Davies <tom@sibylline.xyz>
2024-04-11 14:50:45 +0100

    Navbar CSS cleanup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,32 +37,31 @@ export default function NavBar() {
         </NavigationMenu>
       </div>
       {/*Right Menu Side*/}
-      <div className="border border-gray-200 rounded">
+      <div className="space-x-4">
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
               <Link href="/admin" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                <NavigationMenuLink className="border border-gray-200 items-center justify-center rounded-xl p-4 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                   Admin
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
           </NavigationMenuList>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Button variant="outline" className="p-6 rounded-xl mx-4"
+                      onClick={() => router.push('/collections/LivingIP%20Product/document/cb26aba8-8188-4eb3-867c-2661c260b29c')}>
+                User Guide
+              </Button>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <DynamicWidget innerButtonComponent={<Button>Connect Wallet</Button>}/>
+            </NavigationMenuItem>
+          </NavigationMenuList>
         </NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Button variant="outline"
-                    onClick={() => router.push('/collections/LivingIP%20Product/document/cb26aba8-8188-4eb3-867c-2661c260b29c')}>
-              User Guide
-            </Button>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <DynamicWidget innerButtonComponent={<Button>Connect Wallet</Button>}/>
-          </NavigationMenuItem>
-        </NavigationMenuList>
       </div>
     </div>
   );
