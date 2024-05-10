56f7d459660a99af4b95929fc606d135b9d843fe
Tom Davies <tom@sibylline.xyz>
2024-05-10 17:31:58 +0100

    Change editor updated

--- src/components/EditChangeLayout.jsx
+++ src/components/EditChangeLayout.jsx
@@ -9,9 +9,12 @@ import {
 	AlertDialogTitle
 } from "@/components/ui/alert-dialog";
 import {useStore} from "@/lib/store";
-import {ProfileHeader} from "@/components/custom/ProfileHeader";
+import {Button} from "@/components/ui/button";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import {useRouter} from "next/router";
 
-export function NewLayout({children}) {
+export function EditChangeLayout({children, publishHandler, saveHandler}) {
+	const router = useRouter();
 	const [navbarHeight, setNavbarHeight] = useState(0);
 	const [invalidPermissionsDialogOpen, setInvalidPermissionsDialogOpen] = useStore((state) =>
 		[state.invalidPermissionsDialogOpen, state.setInvalidPermissionsDialogOpen]
@@ -30,7 +33,16 @@ export function NewLayout({children}) {
 
 	return (
 		<div className="flex flex-col pb-6 bg-neutral-100 h-screen px-8">
-			<ProfileHeader/>
+			<div className="flex flex-row justify-between items-center pt-4">
+				<Button variant="ghost" onClick={() => {router.back()}}>
+					<IoIosArrowRoundBack className="w-4 h-4" />
+					Back
+				</Button>
+				<div className="justify-center items-center space-x-2">
+					<Button className="bg-[#E1E5DE] text-[#0A0A0A]" variant="outline" onClick={saveHandler}>Save</Button>
+					<Button onClick={publishHandler}>Publish</Button>
+				</div>
+			</div>
 			{children}
 			<AlertDialog open={invalidPermissionsDialogOpen} onOpenChange={handleDialogClose}>
 				<AlertDialogContent>
--- src/pages/projects/[pid]/document/[documentId]/edit/[id].js
+++ src/pages/projects/[pid]/document/[documentId]/edit/[id].js
@@ -1,92 +1,100 @@
-import { authStytchRequest } from "@/lib/stytch";
+import {authStytchRequest} from "@/lib/stytch";
 import "@mdxeditor/editor/style.css";
-import EditMenu from "@/components/edit/EditMenu";
 import Editor from "@/components/edit/Editor";
-import { useState } from "react";
-import { useRouter } from "next/router";
-import {publishChange, updateChange} from "@/lib/change";
-import { Layout } from "@/components/ui/layout";
+import {useState} from "react";
+import {useRouter} from "next/router";
+import {getChange, publishChange, updateChange} from "@/lib/change";
 import {getProject} from "@/lib/project";
 import {getDocument} from "@/lib/document";
-import {getChange} from "@/lib/change";
 import {getCookie} from "cookies-next";
+import {EditChangeLayout} from "@/components/EditChangeLayout";
+import {FiEdit3} from "react-icons/fi";
 
-export default function Index({ project, document, change }) {
-  const decodedContent = Buffer.from(document.content, 'base64').toString("utf-8");
-  const [pageData, setPageData] = useState(decodedContent);
-  const router = useRouter();
 
-  const editorCallback = (data) => {
-    setPageData(data);
-  };
+export default function Index({project, document, change}) {
+	const decodedContent = Buffer.from(document.content, 'base64').toString("utf-8");
+	const [pageData, setPageData] = useState(decodedContent);
+	const router = useRouter();
 
-  const saveHandler = async () => {
-    console.log("Updating Change", pageData);
-    const response = await updateChange(change.cid, {
-      name: change.name,
-      description: change.description,
-      content: btoa(pageData),
-    }, getCookie("stytch_session_jwt"));
-    console.log(response);
-    //TODO: Update with modal saying successfully saved
-    await router.push(`/projects/${encodeURI(project.pid)}/document/${document.did}`);
-  };
+	const editorCallback = (data) => {
+		setPageData(data);
+	};
 
-  const publishHandler = async () => {
-    console.log("Updating Change", pageData);
-    const updateResponse = await updateChange(change.cid, {
-      name: change.name,
-      description: change.description,
-      content: btoa(pageData),
-    }, getCookie("stytch_session_jwt"));
-    console.log(updateResponse);
-    console.log("Publishing Change", pageData);
-    await publishChange(change.cid, getCookie("stytch_session_jwt"));
-    await router.push(`/projects/${encodeURI(project.pid)}/document/${document.did}/vote`);
-  };
+	const saveHandler = async () => {
+		console.log("Updating Change", pageData);
+		const response = await updateChange(change.cid, {
+			name: change.name,
+			description: change.description,
+			content: btoa(pageData),
+		}, getCookie("stytch_session_jwt"));
+		console.log(response);
+		//TODO: Update with modal saying successfully saved
+		await router.push(`/projects/${encodeURI(project.pid)}/document/${document.did}`);
+	};
 
-  return (
-    <Layout>
-      <div className="flex flex-col w-full h-screen py-8">
-        <div className="relative w-full p-4 border border-gray-300 rounded-lg shadow-lg">
-          <EditMenu saveHandler={saveHandler} publishHandler={publishHandler} />
-          <Editor markdown={pageData} onChange={editorCallback} />
-        </div>
-      </div>
-    </Layout>
-  );
+	const publishHandler = async () => {
+		console.log("Updating Change", pageData);
+		const updateResponse = await updateChange(change.cid, {
+			name: change.name,
+			description: change.description,
+			content: btoa(pageData),
+		}, getCookie("stytch_session_jwt"));
+		console.log(updateResponse);
+		console.log("Publishing Change", pageData);
+		await publishChange(change.cid, getCookie("stytch_session_jwt"));
+		await router.push(`/projects/${encodeURI(project.pid)}/document/${document.did}/vote`);
+	};
+
+	return (
+		<EditChangeLayout saveHandler={saveHandler} publishHandler={publishHandler}>
+			<div className="flex flex-col w-full h-screen py-8">
+				<div className="relative w-full p-4 rounded-lg">
+					<h1 className="flex gap-2 pr-20 text-3xl text-neutral-950 max-md:flex-wrap max-md:pr-5">
+						<span>{document.name}</span>
+						<FiEdit3 className="w-3 h-3 my-auto aspect-square"/>
+					</h1>
+					<section
+						className="flex flex-col p-8 mt-8 text-base bg-white rounded-3xl shadow text-neutral-600 max-md:px-5 max-md:max-w-full">
+						<Editor markdown={pageData} onChange={editorCallback}/>
+					</section>
+				</div>
+			</div>
+		</EditChangeLayout>
+	);
 }
 
-export const getServerSideProps = async ({ req, query }) => {
-  const { session } = await authStytchRequest(req);
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
+export const getServerSideProps = async ({
+	                                         req, query
+                                         }) => {
+	const {session} = await authStytchRequest(req);
+	if (!session) {
+		return {
+			redirect: {
+				destination: "/login",
+				permanent: false,
+			},
+		};
+	}
 
-  const {pid, documentId, id} = query;
-  console.log("Pid: ", pid);
-  console.log("Document ID: ", documentId);
-  console.log("Change ID: ", id);
+	const {pid, documentId, id} = query;
+	console.log("Pid: ", pid);
+	console.log("Document ID: ", documentId);
+	console.log("Change ID: ", id);
 
-  const sessionJWT = req.cookies["stytch_session_jwt"];
+	const sessionJWT = req.cookies["stytch_session_jwt"];
 
-  const project = await getProject(pid, sessionJWT);
-  console.log("Project: ", project);
-  const document = await getDocument(documentId, sessionJWT);
-  console.log("Document: ", document);
-  const change = await getChange(id, sessionJWT);
-  console.log("Change: ", change);
+	const project = await getProject(pid, sessionJWT);
+	console.log("Project: ", project);
+	const document = await getDocument(documentId, sessionJWT);
+	console.log("Document: ", document);
+	const change = await getChange(id, sessionJWT);
+	console.log("Change: ", change);
 
-  return {
-    props: {
-      project: project,
-      document: document,
-      change: change,
-    },
-  };
+	return {
+		props: {
+			project: project,
+			document: document,
+			change: change,
+		},
+	};
 };
