11a99ef1bb775aae992ae5fde12563a0c2b84673
Tom Davies <tom@sibylline.xyz>
2024-05-13 15:27:34 +0100

    commenting out features to be added later

--- src/components/custom/OtherProjectCard.js
+++ src/components/custom/OtherProjectCard.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function OtherProjectCard({project}) {
   return (
     <div
-      className="flex gap-3 py-3 px-3 mt-3 bg-white rounded-xl border border-gray-200 border-solid max-md:flex-wrap">
+      className="flex gap-3 mt-3 bg-white rounded-xl border border-gray-200 border-solid max-md:flex-wrap">
       <div
         className="w-full h-auto max-w-[276px] rounded-xl my-auto max-md:px-5">
         <Image
@@ -16,13 +16,14 @@ export function OtherProjectCard({project}) {
         />
       </div>
       <div className="flex flex-col flex-1 text-sm font-medium max-md:max-w-full">
-        <div className="text-lg leading-7 text-neutral-950 max-md:max-w-full">{project.name}</div>
-        <div className="mt-1 leading-5 text-neutral-600 max-md:max-w-full">{project.description}</div>
-        <div className="flex gap-1 self-start py-3 mt-1 text-xs leading-4 text-lime-950">
-          <div className="justify-center px-2 py-0.5 bg-neutral-300 rounded-[100px]">{project.members} members</div>
-          <div className="justify-center px-2 py-0.5 bg-neutral-300 rounded-[100px]">{project.articles} articles</div>
-        </div>
-        <div className="mt-1 leading-[143%] text-zinc-500 max-md:max-w-full">Last edit {project.lastEdit}</div>
+        <div className="text-lg text-neutral-950 max-md:max-w-full">{project.name}</div>
+        <div className="mt-1 text-neutral-600 max-md:max-w-full">{project.description}</div>
+        {/*TODO: Ben - Uncomment once API returns relevant values*/}
+        {/*<div className="flex gap-1 self-start py-3 mt-1 text-xs text-lime-950">*/}
+        {/*  <div className="justify-center px-2 py-0.5 bg-neutral-300 rounded-[100px]">{project.members} members</div>*/}
+        {/*  <div className="justify-center px-2 py-0.5 bg-neutral-300 rounded-[100px]">{project.articles} articles</div>*/}
+        {/*</div>*/}
+        {/*<div className="mt-1 leading-[143%] text-zinc-500 max-md:max-w-full">Last edit {project.lastEdit}</div>*/}
       </div>
     </div>
   );
