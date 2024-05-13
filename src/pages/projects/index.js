e6d3d00fca38f7604085fc0928f8097bcf94152c
Tom Davies <tom@sibylline.xyz>
2024-05-13 15:35:47 +0100

    filter function added

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,6 +1,5 @@
 import {authStytchRequest} from "@/lib/stytch";
 import {getUserProfile, getUserRoles} from "@/lib/user";
-import {Layout} from "@/components/ui/layout";
 import {useRouter} from "next/router";
 import {getProjects} from "@/lib/project";
 import {initializeStore, useStore} from "@/lib/store";
@@ -9,117 +8,105 @@ import {OtherProjectCard} from "@/components/custom/OtherProjectCard";
 import {NewLayout} from "@/components/NewLayout";
 
 export default function Projects({projects}) {
-  const router = useRouter();
-  const [userRoles, setInvalidPermissionsDialogOpen] = useStore((state) =>
-    [state.userRoles, state.setInvalidPermissionsDialogOpen]
-  );
+	const router = useRouter();
+	const [userRoles, setInvalidPermissionsDialogOpen] = useStore((state) =>
+		[state.userRoles, state.setInvalidPermissionsDialogOpen]
+	);
 
-  const desiredOrder = [
-    "Claynosaurz",
-    "Renaissance Hackathon Demo",
-    "Build Republic",
-    "LivingIP",
-    "LivingIP Product"
-  ];
+	const desiredOrder = [
+		"Claynosaurz",
+		"Renaissance Hackathon Demo",
+		"Build Republic",
+		"LivingIP",
+		"LivingIP Product"
+	];
 
-  const sortedProjects = [...projects].sort((a, b) => {
-    const indexA = desiredOrder.indexOf(a.name);
-    const indexB = desiredOrder.indexOf(b.name);
+	const sortedProjects = [...projects].sort((a, b) => {
+		const indexA = desiredOrder.indexOf(a.name);
+		const indexB = desiredOrder.indexOf(b.name);
 
-    if (indexA === -1 && indexB === -1) {
-      return 0;
-    } else if (indexA === -1) {
-      return 1;
-    } else if (indexB === -1) {
-      return -1;
-    } else {
-      return indexA - indexB;
-    }
-  });
+		if (indexA === -1 && indexB === -1) {
+			return 0;
+		} else if (indexA === -1) {
+			return 1;
+		} else if (indexB === -1) {
+			return -1;
+		} else {
+			return indexA - indexB;
+		}
+	});
 
-  //TODO: YourProjects and OtherProjects should be fetched from the backend
-  const yourProjects = sortedProjects;
-  const otherProjects = [
-    {
-      name: "Build Republic",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      members: 32,
-      articles: 14,
-      lastEdit: "14 April 2024",
-      image_uri: "/collection-covers/living-ip-cover-1.jpeg",
-    },
-    {
-      name: "Kuvera",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      members: 32,
-      articles: 14,
-      lastEdit: "14 April 2024",
-      image_uri: "/collection-covers/living-ip-cover-3.jpeg",
-    },
-  ];
+	// Filter projects based on user roles
+	const yourProjects = sortedProjects.filter(project =>
+		userRoles.find(role => role.project === project.pid)
+	);
 
-  return (
-    <NewLayout>
-      <main className="flex flex-col px-20 py-8 w-full h-auto bg-white rounded-3xl shadow max-md:px-5 max-md:max-w-full">
-        <h1 className="text-3xl text-neutral-950 max-md:max-w-full">Projects</h1>
-        <h2 className="mt-6 text-xl text-neutral-950 max-md:max-w-full">Your projects</h2>
-        <div className="mt-4 max-md:max-w-full">
-          <div className="grid grid-cols-2 2xl:grid-cols-3 gap-5 max-md:grid-cols-1 max-md:gap-0">
-            {yourProjects.map((project, index) => (
-              <YourProjectCard key={index} project={project}/>
-            ))}
-          </div>
-        </div>
-        <h2 className="mt-6 text-xl text-neutral-950 max-md:max-w-full">Other projects</h2>
-        {otherProjects.map((project, index) => (
-          <OtherProjectCard key={index} project={project}/>
-        ))}
-      </main>
-    </NewLayout>
-  );
+// Projects in which the user doesn't have a role
+	const otherProjects = sortedProjects.filter(project =>
+		!userRoles.find(role => role.project === project.pid)
+	);
+
+	return (
+		<NewLayout>
+			<main
+				className="flex flex-col px-20 py-8 w-full h-auto bg-white rounded-3xl shadow max-md:px-5 max-md:max-w-full">
+				<h1 className="text-3xl text-neutral-950 max-md:max-w-full">Projects</h1>
+				<h2 className="mt-6 text-xl text-neutral-950 max-md:max-w-full">Your projects</h2>
+				<div className="mt-4 max-md:max-w-full">
+					<div className="grid grid-cols-2 2xl:grid-cols-3 gap-5 max-md:grid-cols-1 max-md:gap-0">
+						{yourProjects.map((project, index) => (
+							<YourProjectCard key={index} project={project}/>
+						))}
+					</div>
+				</div>
+				<h2 className="mt-6 text-xl text-neutral-950 max-md:max-w-full">Other projects</h2>
+				{otherProjects.map((project, index) => (
+					<OtherProjectCard key={index} project={project}/>
+				))}
+			</main>
+		</NewLayout>
+	);
 }
 
 export const getServerSideProps = async ({req}) => {
-  const {session} = await authStytchRequest(req);
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
-  const sessionJWT = req.cookies["stytch_session_jwt"];
-  const {userProfile} = await getUserProfile(session.user_id, sessionJWT);
-  if (!userProfile) {
-    return {
-      redirect: {
-        destination: "/onboard",
-        permanent: false,
-      },
-    };
-  }
+	const {session} = await authStytchRequest(req);
+	if (!session) {
+		return {
+			redirect: {
+				destination: "/login",
+				permanent: false,
+			},
+		};
+	}
+	const sessionJWT = req.cookies["stytch_session_jwt"];
+	const {userProfile} = await getUserProfile(session.user_id, sessionJWT);
+	if (!userProfile) {
+		return {
+			redirect: {
+				destination: "/onboard",
+				permanent: false,
+			},
+		};
+	}
 
-  const projects = await getProjects(sessionJWT);
-  console.log("Projects: ", projects);
+	const projects = await getProjects(sessionJWT);
+	console.log("Projects: ", projects);
 
-  const userRoles = await getUserRoles(session.user_id, sessionJWT);
-  console.log("User Roles: ", userRoles);
+	const userRoles = await getUserRoles(session.user_id, sessionJWT);
+	console.log("User Roles: ", userRoles);
 
-  const zustandServerStore = initializeStore({
-    userProfile,
-    userRoles,
-    currentProject: undefined,
-  });
+	const zustandServerStore = initializeStore({
+		userProfile,
+		userRoles,
+		currentProject: undefined,
+	});
 
-  return {
-    props: {
-      projects,
-      initialZustandState: JSON.parse(
-        JSON.stringify(zustandServerStore.getState())
-      ),
-    },
-  };
+	return {
+		props: {
+			projects,
+			initialZustandState: JSON.parse(
+				JSON.stringify(zustandServerStore.getState())
+			),
+		},
+	};
 };
