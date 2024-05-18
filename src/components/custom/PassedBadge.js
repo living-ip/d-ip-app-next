5748c78f1429b52b31a9b84d71e75879f61e0bde
Tom Davies <tom@sibylline.xyz>
2024-05-18 13:50:25 +0100

    log history. requires information from backend

---
+++ src/components/custom/PassedBadge.js
@@ -0,0 +1,7 @@
+export function PassedBadge() {
+	return (
+		<div className="justify-center items-center h-fit px-2 py-0.5 rounded-lg text-sm bg-green-300 text-green-600">
+			Published
+		</div>
+	);
+}
---
+++ src/components/custom/RejectedBadge.js
@@ -0,0 +1,7 @@
+export function RejectedBadge() {
+	return (
+		<div className="justify-center items-center h-fit px-2 py-0.5 rounded-lg text-sm bg-red-300 text-red-600">
+			Published
+		</div>
+	);
+}
