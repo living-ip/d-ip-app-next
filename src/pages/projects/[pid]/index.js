985be4270fc09a27d81e8164db261167d94adc1e
Dan Miles <dan@kilometre.dev>
2024-07-04 11:38:25 +0100

    remove side box cta

--- src/pages/projects/[pid]/index.js
+++ src/pages/projects/[pid]/index.js
@@ -1,150 +1,132 @@
-import {useRouter} from "next/router";
-import {Button} from "@/components/ui/button";
-import {authStytchRequest} from "@/lib/stytch";
-import {getUserProfile, getUserRoles} from "@/lib/user";
-import {getProject, getProjectDocuments} from "@/lib/project";
-import {initializeStore, useStore} from "@/lib/store";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
-import {DocumentCard} from "@/components/custom/DocumentCard";
-import {NewLayout} from "@/components/NewLayout";
+import { useRouter } from "next/router";
+import { Button } from "@/components/ui/button";
+import { authStytchRequest } from "@/lib/stytch";
+import { getUserProfile, getUserRoles } from "@/lib/user";
+import { getProject, getProjectDocuments } from "@/lib/project";
+import { initializeStore, useStore } from "@/lib/store";
+import { IoArrowBackOutline } from "react-icons/io5";
+import { DocumentCard } from "@/components/custom/DocumentCard";
+import { NewLayout } from "@/components/NewLayout";
 
-const ProjectHeader = ({projectName, contributorCount}) => {
-	const router = useRouter();
+const ProjectHeader = ({ projectName, contributorCount }) => {
+  const router = useRouter();
 
-	return (
-		<div className="flex gap-3 items-center">
-			<div className="justify-center items-center p-2.5 my-auto rounded-sm border border-gray-200 border-solid"
-			     onClick={() => router.back()}>
-				<FontAwesomeIcon icon={faArrowLeft} className="w-3 aspect-square"/>
-			</div>
-			<h1 className="text-3xl leading-9 text-neutral-950">{projectName}</h1>
-			{/*TODO: Ben - uncomment when contributorCount is returned*/}
-			{/*<ContributorBadge contributorCount={contributorCount}/>*/}
-		</div>
-	);
+  return (
+    <div className="flex gap-3 items-center">
+      <Button variant="outline" className="p-2.5 rounded-sm border border-gray-200 border-solid">
+        <IoArrowBackOutline className="w-4 h-4 cursor-pointer" onClick={() => router.back()} />
+      </Button>
+      <h1 className="text-3xl leading-9 text-neutral-950">{projectName}</h1>
+      {/*TODO: Ben - uncomment when contributorCount is returned*/}
+      {/*<ContributorBadge contributorCount={contributorCount}/>*/}
+    </div>
+  );
 }
 
-
-const ProjectDescription = ({description}) => (
-	<p className="mt-3 text-sm leading-5 text-neutral-600 w-[600px] max-md:max-w-full">{description}</p>
+const ProjectDescription = ({ description }) => (
+  <p className="mt-3 text-base leading-6 text-neutral-600 max-md:max-w-full">{description}</p>
 );
 
+const DocumentCards = ({ project, documents }) => {
+  const router = useRouter();
+  const [userRoles, setInvalidPermissionsDialogOpen] = useStore((state) => [
+    state.userRoles,
+    state.setInvalidPermissionsDialogOpen
+  ]);
 
-const DocumentCards = ({project, documents}) => {
-	const router = useRouter();
-	const [userRoles, setInvalidPermissionsDialogOpen] = useStore((state) =>
-		[state.userRoles, state.setInvalidPermissionsDialogOpen]
-	);
+  const handleCreateNewDocument = () => {
+    if (!userRoles.find((role) => role.project === project.pid && role.role.create_document)) {
+      setInvalidPermissionsDialogOpen(true);
+      return;
+    }
+    router.push(`/projects/${encodeURI(project.pid)}/new`);
+  };
 
-	console.log("Project: ", project);
-	console.log("Documents: ", documents);
+  //TODO: Ben - Update contributorCount to correct count from API
+  const contributorCount = "XX";
 
-	const RequestNewDocCTA = () => (
-		<div
-			className="flex flex-col justify-center items-center self-stretch p-8 w-full rounded-xl bg-neutral-100 text-neutral-950 max-md:px-5 max-md:mt-8">
-			<h2 className="text-2xl leading-8 text-center">Want more content?</h2>
-			<p className="self-stretch mt-2 text-sm leading-5 text-center text-neutral-600">
-				If you notice something missing, get involved and make a new document!
-			</p>
-			<Button
-				className="justify-center px-3 py-2 mt-4 font-medium leading-6 rounded text-neutral-950 bg-[#E1E5DE]"
-				onClick={() => {
-					if (!userRoles.find((role) => role.project === project.pid && role.role.create_document)) {
-						setInvalidPermissionsDialogOpen(true);
-						return;
-					}
-					router.push(`/projects/${encodeURI(project.pid)}/new`);
-				}}
-			>
-				Create New Document
-			</Button>
-		</div>
-	);
-
-	//TODO: Ben - Update contributorCount to correct count from API
-	const contributorCount = "XX";
-
-	return (
-		<NewLayout>
-			<main
-				className="flex flex-col items-start self-center px-20 max-md:px-2 py-8 max-md:py-2 w-full bg-white rounded-3xl shadow">
-				<ProjectHeader projectName={project.name} contributorCount={contributorCount}/>
-				<ProjectDescription description={project.description}/>
-				<div className="self-stretch mt-6 mb-14 max-md:mb-10 max-md:max-w-full">
-					<div className="flex gap-5 max-md:flex-col max-md:gap-0">
-						<section className="flex flex-col w-[73%] max-md:ml-0 max-md:w-full">
-							<div className="flex flex-col grow text-sm max-md:mt-8 max-md:max-w-full">
-								<h2 className="text-xl leading-7 text-neutral-950 max-md:max-w-full">Documents</h2>
-								{documents.map((document, index) => (
-									// TODO: Ben - Update lastEditDate to correct date field from API
-									<DocumentCard key={index} name={document.name}
-									              description={document.description}
-									              lastEditDate={document.created_at}
-									              onClick={() => router.push(`/projects/${encodeURI(project.pid)}/document/${document.did}`)}
-									/>
-								))}
-							</div>
-						</section>
-						<aside className="flex flex-col ml-5 w-[27%] max-md:ml-0 max-md:w-full">
-							<RequestNewDocCTA/>
-						</aside>
-					</div>
-				</div>
-			</main>
-		</NewLayout>
-	);
+  return (
+    <NewLayout>
+      <main className="flex flex-col self-center px-20 py-8 w-full bg-white rounded-3xl shadow max-md:px-5 max-md:max-w-full">
+        <section className="flex flex-row max-md:flex-col gap-3 justify-between max-md:justify-center w-full">
+          <div className="flex flex-col w-[73%] max-md:w-full">
+            <ProjectHeader projectName={project.name} contributorCount={contributorCount} />
+            <ProjectDescription description={project.description} />
+          </div>
+          <div className="flex justify-end items-center gap-3 w-[27%] max-md:w-[100%] mt-24 max-md:mt-2">
+            <Button variant="outline" disabled={true}>
+              Log History
+            </Button>
+            <Button onClick={handleCreateNewDocument}>
+              Create New Document
+            </Button>
+          </div>
+        </section>
+        <article className="mt-6 max-md:max-w-full">
+          <div className="flex flex-col w-full">
+            <h2 className="text-xl leading-7 text-neutral-950 mb-4">Documents</h2>
+            <div className="flex flex-col gap-4">
+              {documents.map((document, index) => (
+                <DocumentCard
+                  key={index}
+                  name={document.name}
+                  description={document.description}
+                  lastEditDate={document.created_at}
+                  onClick={() => router.push(`/projects/${encodeURI(project.pid)}/document/${document.did}`)}
+                />
+              ))}
+            </div>
+          </div>
+        </article>
+      </main>
+    </NewLayout>
+  );
 };
 
 export default DocumentCards;
 
-export const getServerSideProps = async ({req, query}) => {
-	const {session} = await authStytchRequest(req);
-	if (!session) {
-		return {
-			redirect: {
-				destination: "/login",
-				permanent: false,
-			},
-		};
-	}
-	const sessionJWT = req.cookies["stytch_session_jwt"];
-	const {userProfile, roles} = await getUserProfile(session.user_id, sessionJWT);
-	if (!userProfile) {
-		return {
-			redirect: {
-				destination: "/onboard",
-				permanent: false,
-			},
-		};
-	}
+export const getServerSideProps = async ({ req, query }) => {
+  const { session } = await authStytchRequest(req);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+  const sessionJWT = req.cookies["stytch_session_jwt"];
+  const { userProfile, roles } = await getUserProfile(session.user_id, sessionJWT);
+  if (!userProfile) {
+    return {
+      redirect: {
+        destination: "/onboard",
+        permanent: false,
+      },
+    };
+  }
 
-	const {pid} = query;
-	console.log("pid: ", pid)
-	const project = await getProject(pid, sessionJWT);
-	console.log("Project: ", project)
-	if (!project) {
-		return {
-			redirect: {
-				destination: "/projects",
-				permanent: false,
-			},
-		};
-	}
-	const documents = await getProjectDocuments(project.pid, sessionJWT)
-	console.log("Documents: ", documents);
-	const zustandServerStore = initializeStore({
-		userProfile,
-		userRoles: roles,
-		currentProject: pid,
-	});
-	return {
-		props: {
-			project: project,
-			documents: documents,
-			initialZustandState: JSON.parse(
-				JSON.stringify(zustandServerStore.getState())
-			),
-		},
-	};
+  const { pid } = query;
+  const project = await getProject(pid, sessionJWT);
+  if (!project) {
+    return {
+      redirect: {
+        destination: "/projects",
+        permanent: false,
+      },
+    };
+  }
+  const documents = await getProjectDocuments(project.pid, sessionJWT);
+  const zustandServerStore = initializeStore({
+    userProfile,
+    userRoles: roles,
+    currentProject: pid,
+  });
+  return {
+    props: {
+      project: project,
+      documents: documents,
+      initialZustandState: JSON.parse(JSON.stringify(zustandServerStore.getState())),
+    },
+  };
 };
