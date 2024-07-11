bab9026f3d6b2d79f610179eb91818a81a9ad1c3
Dan Miles <dan@kilometre.dev>
2024-07-12 00:48:52 +0100

    timestamps

--- src/components/custom/VotePageBadge.js
+++ src/components/custom/VotePageBadge.js
@@ -1,7 +1,7 @@
-export function VotingOngoingBadge() {
+export function VotePageBadge({children}) {
 	return (
 		<section className="justify-center px-3 py-0.5 text-sm font-medium bg-[#E8ECE6] rounded-full text-neutral-950">
-			Voting ongoing
+			{children}
 		</section>
 	)
 }
--- src/pages/projects/[pid]/document/[documentId]/vote/[id].js
+++ src/pages/projects/[pid]/document/[documentId]/vote/[id].js
@@ -5,15 +5,19 @@ import "react-diff-view/style/index.css";
 import {getChange, getChangeVotes} from "@/lib/change";
 import {getProject} from "@/lib/project";
 import {getDocument} from "@/lib/document";
-import {getUserProfile, getUserRoles} from "@/lib/user";
+import {getUserProfile} from "@/lib/user";
 import {initializeStore, useStore} from "@/lib/store";
 import {VotingForm} from "@/components/custom/VotingForm";
 import {NewLayout} from "@/components/NewLayout";
-import {VotingOngoingBadge} from "@/components/custom/VotingOngoingBadge";
+import {VotePageBadge} from "@/components/custom/VotePageBadge";
 import {ResultsCard} from "@/components/custom/ResultsCard";
 import {AwaitResults} from "@/components/custom/AwaitResults";
+import {Button} from "@/components/ui/button";
+import {IoArrowBackOutline} from "react-icons/io5";
+import {useRouter} from "next/router";
 
 export default function Index({project, document, change, changeVotes, userVoteProp}) {
+	const router = useRouter();
 	const [userRoles, setInvalidPermissionsDialogOpen] = useStore((state) =>
 		[state.userRoles, state.setInvalidPermissionsDialogOpen]
 	);
@@ -35,6 +39,22 @@ export default function Index({project, document, change, changeVotes, userVoteP
 		</Diff>
 	);
 
+	const getTimeBadge = (remainingTime) => {
+		const minutes = Math.floor(remainingTime / 60000);
+		const hours = Math.floor(minutes / 60);
+		const days = Math.floor(hours / 24);
+
+		if (remainingTime <= 0) {
+			return "Expired";
+		} else if (minutes < 60) {
+			return `${minutes} min${minutes !== 1 ? 's' : ''}`;
+		} else if (hours < 24) {
+			return `${hours} hr${hours !== 1 ? 's' : ''}`;
+		} else {
+			return `${days} day${days !== 1 ? 's' : ''}`;
+		}
+	}
+
 	const userCanVote = () => {
 		if (!userRoles.find((role) => role.project === project.pid && role.role.vote_on_change)) {
 			setInvalidPermissionsDialogOpen(true);
@@ -55,8 +75,18 @@ export default function Index({project, document, change, changeVotes, userVoteP
 			<div className="flex flex-col justify-center pb-6 bg-neutral-100">
 				<main
 					className="flex flex-col items-start p-8 w-full bg-white rounded-3xl max-md:px-5 max-md:max-w-full">
-					<VotingOngoingBadge/>
-					<h1 className="mt-7 text-3xl text-neutral-950 max-md:max-w-full">{change.name}</h1>
+					<div className="flex gap-3 max-md:flex-wrap">
+						<Button variant="outline" className="p-2.5 rounded-sm border border-gray-200 border-solid">
+							<IoArrowBackOutline className="w-4 h-4 cursor-pointer" onClick={() => router.back()}/>
+						</Button>
+						<div className="text-3xl leading-9 text-neutral-950 max-md:max-w-full">{document.name}</div>
+					</div>
+					<div className={"flex mt-2 space-x-2"}>
+						<VotePageBadge>Vote Ongoing</VotePageBadge>
+						{change.time_left_ms && (
+							<VotePageBadge>{getTimeBadge(change.time_left_ms)}</VotePageBadge>
+						)}
+					</div>
 					<p className="mt-2 text-sm text-neutral-600 w-[722px] max-md:max-w-full">{change.description}</p>
 					<section className="mt-7 mb-40 max-md:mb-10 max-md:max-w-full">
 						<div className="flex gap-5 max-md:flex-col max-md:gap-0">
@@ -70,7 +100,7 @@ export default function Index({project, document, change, changeVotes, userVoteP
 							</article>
 							<aside className="flex flex-col ml-5 w-[31%] max-md:ml-0 max-md:w-full">
 								{(change.closed || change.merged) ? (
-									<ResultsCard change={change} changeVotes={changeVotes} />
+									<ResultsCard change={change} changeVotes={changeVotes}/>
 								) : (
 									userCanVote() ? (
 										<>
@@ -95,66 +125,66 @@ export default function Index({project, document, change, changeVotes, userVoteP
 }
 
 export const getServerSideProps = async ({req, query}) => {
-    const {session} = await authStytchRequest(req);
-    if (!session) {
-        return {
-            redirect: {
-                destination: "/login",
-                permanent: false,
-            },
-        };
-    }
+	const {session} = await authStytchRequest(req);
+	if (!session) {
+		return {
+			redirect: {
+				destination: "/login",
+				permanent: false,
+			},
+		};
+	}
 
-    const {pid, documentId, id} = query;
-    console.log("Pid: ", pid);
-    console.log("Document ID: ", documentId);
-    console.log("Change ID: ", id);
+	const {pid, documentId, id} = query;
+	console.log("Pid: ", pid);
+	console.log("Document ID: ", documentId);
+	console.log("Change ID: ", id);
 
-    const sessionJWT = req.cookies["stytch_session_jwt"];
+	const sessionJWT = req.cookies["stytch_session_jwt"];
 
-    // Execute the following requests in parallel
-    const [project, document, change, changeVotes] = await Promise.all([
-        getProject(pid, sessionJWT),
-        getDocument(documentId, sessionJWT),
-        getChange(id, sessionJWT),
-        getChangeVotes(id, {"include_voters": true}, sessionJWT)
-    ]);
+	// Execute the following requests in parallel
+	const [project, document, change, changeVotes] = await Promise.all([
+		getProject(pid, sessionJWT),
+		getDocument(documentId, sessionJWT),
+		getChange(id, sessionJWT),
+		getChangeVotes(id, {"include_voters": true}, sessionJWT)
+	]);
 
-    console.log("Project: ", project);
-    console.log("Document: ", document);
-    console.log("Change: ", change);
-    console.log("Change Votes: ", changeVotes);
+	console.log("Project: ", project);
+	console.log("Document: ", document);
+	console.log("Change: ", change);
+	console.log("Change Votes: ", changeVotes);
 
-    if (!project || !document || !change) {
-        return {
-            redirect: {
-                destination: `/projects/${pid}/document/${documentId}`,
-                permanent: false,
-            },
-        };
-    }
+	if (!project || !document || !change) {
+		return {
+			redirect: {
+				destination: `/projects/${pid}/document/${documentId}`,
+				permanent: false,
+			},
+		};
+	}
 
-    const userVote = changeVotes.voters.find((vote) => vote.voter_id === session.user_id);
-    console.log("User Vote: ", userVote);
+	const userVote = changeVotes.voters.find((vote) => vote.voter_id === session.user_id);
+	console.log("User Vote: ", userVote);
 
-    const {userProfile, roles} = await getUserProfile(session.user_id, sessionJWT);
+	const {userProfile, roles} = await getUserProfile(session.user_id, sessionJWT);
 
-    const zustandServerStore = initializeStore({
-        userProfile,
-        userRoles: roles,
-        currentProject: pid,
-    });
+	const zustandServerStore = initializeStore({
+		userProfile,
+		userRoles: roles,
+		currentProject: pid,
+	});
 
-    return {
-        props: {
-            project: project,
-            document: document,
-            change: change,
-            changeVotes: changeVotes || {},
-            userVoteProp: userVote ? userVote.vote : 0,
-            initialZustandState: JSON.parse(
-                JSON.stringify(zustandServerStore.getState())
-            ),
-        },
-    };
+	return {
+		props: {
+			project: project,
+			document: document,
+			change: change,
+			changeVotes: changeVotes || {},
+			userVoteProp: userVote ? userVote.vote : 0,
+			initialZustandState: JSON.parse(
+				JSON.stringify(zustandServerStore.getState())
+			),
+		},
+	};
 };
