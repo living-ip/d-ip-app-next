108db0fa06ab4cca724d32479ab05918715adfa6
Dan Miles <dan@kilometre.dev>
2024-07-23 15:37:45 +0100

    fix projects card height

--- src/components/custom/YourProjectCard.js
+++ src/components/custom/YourProjectCard.js
@@ -20,9 +20,9 @@ export function YourProjectCard({ project }) {
   return (
     <div
       onClick={goToProject}
-      className="flex flex-col w-full cursor-pointer"
+      className="flex flex-col w-full h-[360px] cursor-pointer"
     >
-      <div className="flex flex-col w-full rounded-xl border border-gray-200 border-solid overflow-hidden">
+      <div className="flex flex-col w-full h-full rounded-xl border border-gray-200 border-solid overflow-hidden">
         <div className="w-full h-48 relative">
           <Image
             className="object-cover object-center"
@@ -32,11 +32,11 @@ export function YourProjectCard({ project }) {
             sizes="(max-width: 768px) 100vw, 33vw"
           />
         </div>
-        <div className="flex flex-col p-4">
-          <h3 className="mb-2 text-lg font-medium leading-7 text-neutral-950">
+        <div className="flex flex-col p-4 h-[168px]">
+          <h3 className="mb-2 text-lg font-medium leading-7 text-neutral-950 line-clamp-1">
             {project.name}
           </h3>
-          <p className="text-sm leading-5 text-neutral-600 overflow-x-clip">
+          <p className="text-sm leading-5 text-neutral-600 line-clamp-4">
             {project.description}
           </p>
         </div>
