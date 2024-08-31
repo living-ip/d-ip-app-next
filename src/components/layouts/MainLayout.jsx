3a91c042ea2b281e8e175b919122c5e108acb90a
Dan Miles <dan@kilometre.dev>
2024-08-31 20:02:21 +0100

    feat: Add MainLayout component

---
+++ src/components/layouts/MainLayout.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle
+} from "@/components/ui/alert-dialog";
+import {useStore} from "@/lib/store";
+import {NewNavBar} from "@/components/custom/NewNavBar";
+
+export function MainLayout({children}) {
+	const [invalidPermissionsDialogOpen, setInvalidPermissionsDialogOpen] = useStore((state) => [
+		state.invalidPermissionsDialogOpen,
+		state.setInvalidPermissionsDialogOpen
+	]);
+	const handleDialogClose = () => {
+		setInvalidPermissionsDialogOpen(false);
+	}
+	return (
+		<div className="flex flex-col min-h-screen bg-neutral-100">
+			<NewNavBar/>
+			<main className="flex-grow px-4 md:px-8 py-6 overflow-y-auto">
+				{children}
+			</main>
+			<AlertDialog open={invalidPermissionsDialogOpen} onOpenChange={handleDialogClose}>
+				<AlertDialogContent>
+					<AlertDialogHeader>
+						<AlertDialogTitle>
+							Invalid Permissions
+						</AlertDialogTitle>
+						<AlertDialogDescription>
+							You do not have permissions to perform this action.
+						</AlertDialogDescription>
+					</AlertDialogHeader>
+					<AlertDialogFooter>
+						<AlertDialogAction onClick={handleDialogClose}>
+							OK
+						</AlertDialogAction>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialog>
+		</div>
+	)
+}
